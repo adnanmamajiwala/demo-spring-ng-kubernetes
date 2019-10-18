a36d929424fcbc4734b6825cd7fac93c60b597ac
adnanmamajiwala <rawkz.star@gmail.com>
2019-10-18 14:57:04 -0500

    Extracts the application config initializer creates mechanism to set local profile when running angular app in standalone

--- employee-app/employee-ui/src/app/app.module.ts
+++ employee-app/employee-ui/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {EmployeesComponent} from './employees/employees.component';
-import {initApp} from "./app.configuration";
+import {AppConfigInitializer} from "./app.configuration";
 
 @NgModule({
   declarations: [
@@ -18,12 +18,7 @@ import {initApp} from "./app.configuration";
     AppRoutingModule
   ],
   providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initApp,
-      multi: true,
-      deps: [HttpClient]
-    }
+    AppConfigInitializer
   ],
   bootstrap: [AppComponent]
 })
