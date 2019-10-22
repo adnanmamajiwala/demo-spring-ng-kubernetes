9242f2b6805bb11d11d00095894c31b0412ef2db
adnanmamajiwala <rawkz.star@gmail.com>
2019-10-22 12:02:10 -0500

    Adds deployment and service yml's for kubernetes - replaces eureka with kubernetes - adds clusterbinding and ingress yml for the entire project - add docker files

--- employee-app/employee-ui/src/app/configurations/configuration-prod.ts
+++ employee-app/employee-ui/src/app/configurations/configuration-prod.ts
@@ -1,5 +1,5 @@
 import {Configuration} from "./configuration";
 
 export class ConfigurationProd implements Configuration {
-  readonly employees_url = 'http://localhost:9020/employees';
+  readonly employees_url = 'http://employee.innoventing.net/employees';
 }
